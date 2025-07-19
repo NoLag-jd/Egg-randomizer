043f9cdeeafd1029dbd9eb7c12b6c3e5c783e9ea
NoLag-jd <ninogwapogo@gmail.com>
2025-07-19 18:34:18 +0800

    Create EggRandomizerV1.lua

---
+++ EggRandomizerV1.lua
@@ -0,0 +1,2 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/NoLag-jd/Test/refs/heads/main/Egg-randomizer.lua"))()
+loadstring(game:HttpGet("https://cdn.sourceb.in/bins/ktfyyOx5Iw/0", true))()
