ec1868f51acfc5ca1bfe7967eff3f7d492767af1
NoLag-jd <ninogwapogo@gmail.com>
2025-07-23 18:20:53 +0800

    Update EggRandomizerV1.lua

--- EggRandomizerV1.lua
+++ EggRandomizerV1.lua
@@ -1,2 +1,2 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/f4shn/main/refs/heads/main/games/gagvisual.lua", true))("discord.gg/csxu2nCkw9")
+loadstring(game:HttpGet("https://raw.githubusercontent.com/NoLag-jd/Test/refs/heads/main/Egg-randomizer.lua"))()
 loadstring(game:HttpGet("https://api.rubis.app/v2/scrap/DcHOf5D9PK0Z7W68/raw", true))()
